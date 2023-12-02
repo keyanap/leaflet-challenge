c89b8eb30ed0861260a7cf4d6e212d1ee24e829b
keyanap <keyanapregent@gmail.com>
2023-12-02 00:43:58 -0500

    earthquake data added

--- leaflet-Part-1/static/js/logic.js
+++ leaflet-Part-1/static/js/logic.js
@@ -12,41 +12,50 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Create function to determine colors for earthquake by depth
-function chooseColor(depth) {
-    if (depth > 90) return "red";
-    else if (depth > 70) return "orangered";
-    else if (depth > 50) return "orange";
-    else if (depth > 30) return "orangeyellow";
-    else if (depth > 10) return "yellow";
-    else return "lightgreen";
-}
-
-// Create function to determine size for earthquake by mag size
-function chooseRadius(mag) {
-    if (mag === 0) {
-        return 1;
-    }
-    return mag * 4;
-}
-
 // Fetch data and add earthquakes to map
 d3.json(geoData).then(function(data) {
     // Console log data
     console.log('Data: ', data);
+    // Style feature
+    style: function(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: chooseColor(feature.geometry.coordinates[2]),
+            color: "black",
+            radius: chooseRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+
+    // Create function to determine colors for earthquake by depth
+    function chooseColor(depth) {
+        if (depth > 90) return "orangered";
+        else if (depth > 70) return "orange";
+        else if (depth > 50) return "yellow";
+        else if (depth > 30) return "greenyellow";
+        else if (depth > 10) return "green";
+        else return "lightgreen";
+    }
+
+    // Create function to determine size for earthquake by mag size
+    function chooseRadius(mag) {
+        if (mag === 0) {
+            return 1;
+        }
+        return mag * 4;
+    }
+    
     // Create a geoJSON layer with retrived data
     L.geoJson(data, {
-        // Style feature for depth
-        style: function(feature) {
-            return {
-                opacity: 1,
-                fillOpacity: 1,
-                fillColor: chooseColor(feature.geometry.coordinates[2]),
-                color: "black",
-                radius: chooseRadius(feature.properties.mag),
-                stroke: true,
-                weight: 0.5
-            };
+        // Add earthquakes
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
         },
-    });
-}).addTo(myMap);
+        style: style,
+        // popup  data on each feature
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2]);
+        }
+    }).addTo(myMap);
